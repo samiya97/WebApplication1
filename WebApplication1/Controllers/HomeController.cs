5c3439111081336f228494b0b21636b04dc20336
samiya97 <35106772+samiya97@users.noreply.github.com>
2018-09-14 15:53:02 +0000

    Update HomeController.cs

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index2()
         {
             return View();
         }
